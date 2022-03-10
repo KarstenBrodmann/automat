530a137ce2d9678b2c725b95a79a75898b2d0921
Karsten Brodmann <karsten.brodmann@osnanet.de>
2022-03-10 21:29:10 +0100

    Neuer Versuch mit VS Code

--- Code/Automat.java
+++ Code/Automat.java
@@ -5,25 +5,25 @@ import AlgoTools.IO;
  */
 public class Automat {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    int[][] delta = { // Überführungsfunktion
-      {0, 1},       
-      {2, 0},
-      {1, 2}
-    };
-    int s = 0;                     // Startzustand
-    char[] zeile = IO.readChars(); // Eingabezeichen
+        int[][] delta = {              // Überführungsfunktion
+            {0, 1},             
+            {2, 0},
+            {1, 2}
+        };
+        int s = 0;                     // Startzustand
+        char[] zeile = IO.readChars(); // Eingabezeichen
 
-    // Überführungsfunktion zeichenweise anwenden
-    for (int i = 0; i < zeile.length; i++)
-      s = delta[s][zeile[i] - '0'];
+        // Überführungsfunktion zeichenweise anwenden
+        for (int i = 0; i < zeile.length; i++)
+            s = delta[s][zeile[i] - '0'];
 
-    // Endzustand prüfen
-    if (s == 0)
-      IO.println("Eingabe ist durch 3 teilbar.");
-    else
-      IO.println("Eingabe ist NICHT durch 3 teilbar.");
-  }
+        // Endzustand prüfen
+        if (s == 0)
+            IO.println("Eingabe ist durch 3 teilbar.");
+        else
+            IO.println("Eingabe ist NICHT durch 3 teilbar.");
+    }
 
 }
